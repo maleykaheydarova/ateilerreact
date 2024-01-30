bb80b3513ac13fd9183bbfbae42b6567cb1bf476
maleykaheydarova <mheyderova11@gmail.com>
2024-01-30 14:25:26 +0400

    initial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { AiFillAlert } from "react-icons/ai";
+
+function App() {
+  return (
+    <div className="App">
+     <AiFillAlert />
+    </div>
+  );
+}
+
+export default App;
