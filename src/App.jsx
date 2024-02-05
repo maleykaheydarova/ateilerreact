b10aeca8eeeddde04f8ff92841ddd1f1a5fcaa4e
maleykaheydarova <mheyderova11@gmail.com>
2024-02-05 12:10:09 +0400

    l

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "../node_modules/@fortawesome/fontawesome-free/css/solid.min.css"
 import "../node_modules/@fortawesome/fontawesome-free/css/brands.min.css"
 import "../node_modules/@fortawesome/fontawesome-free/css/fontawesome.min.css"
-import "./assets/scss/main.scss"
+import "./assets/scss/_main.scss"
 
 
 function App() {
