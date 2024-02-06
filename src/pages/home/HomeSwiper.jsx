ba08b028c2677b0de16c9b959ce817631ff82208
CamaladdinAhmadzada <ecemaleddin49@gmail.com>
2024-02-06 17:44:14 +0400

    swiper and mobmenu

--- src/pages/home/HomeSwiper.jsx
+++ src/pages/home/HomeSwiper.jsx
@@ -65,3 +65,71 @@
 // }
 
 // export default HomeSwiper
+
+
+import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+
+
+const HomeSwiper = () => {
+    return (
+
+        // Import Swiper styles
+
+        <Swiper
+            spaceBetween={50}
+            slidesPerView={1}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+        >
+            <SwiperSlide>
+                <div class="slider slider-achil">
+                    <div class="container d-flex flex-column gap-3">
+                        <p class="slider-title">
+                            New Achilles Mid
+                            <br />
+                            is now available.
+                        </p>
+                        <p class="learn-btn">
+                            <a href="#">
+                                <span>Learn More</span>
+                                <i class="fa-solid fa-arrow-right"></i>
+                            </a>
+                        </p>
+                        <p class="add-to-cart-btn">
+                            <a href="#">
+                                <i class="fa-solid fa-cart-plus"></i>
+                                <span>Add To Cart</span>
+                            </a>
+                        </p>
+                    </div>
+                </div>
+            </SwiperSlide>
+            <SwiperSlide>
+                <div class="slider slider-ateiler">
+                    <div class="slider-opac"></div>
+                    <div class="container d-flex flex-column gap-3 justify-content-center align-items-center">
+                        <div class="slider-text">
+                            <p>Inside Atelier: A Snea Reek</p>
+                            <p>Into Our Amazing Shop</p>
+                        </div>
+                        <p class="learn-more-btn">
+                            <a href="#">
+                                <span>LEARN MORE</span>
+                                <i class="fa-solid fa-arrow-right"></i>
+                            </a>
+                        </p>
+                    </div>
+                </div>
+            </SwiperSlide>
+            <SwiperSlide>
+                <div class="slider slider-workshop">
+                    <img src={require('../../assets/img/swiper-img.png')} />
+                </div>
+            </SwiperSlide>
+        </Swiper>
+    )
+};
+
+export default HomeSwiper
