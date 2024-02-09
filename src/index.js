b40904c4cac50040e1733bab425b9a9c4e8aa2a2
maleykaheydarova <mheyderova11@gmail.com>
2024-02-09 18:12:18 +0400

    tyhujihgf

--- src/index.js
+++ src/index.js
@@ -12,6 +12,6 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-// ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App/>, document.getElementById("root"));
 
 
